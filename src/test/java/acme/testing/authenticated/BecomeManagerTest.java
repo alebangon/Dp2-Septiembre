71974da4e6894121c08078be9023823569480320
Alejandro Baños Gonzalez <alebangon@alum.us.es>
2021-08-18 12:56:38 +0200

    Arreglado test become Manager

--- src/test/java/acme/testing/authenticated/BecomeManagerTest.java
+++ src/test/java/acme/testing/authenticated/BecomeManagerTest.java
@@ -3,17 +3,27 @@ package acme.testing.authenticated;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
-import acme.testing.SignUpTest;
+import acme.testing.AcmePlannerTest;
 
-public class BecomeManagerTest extends SignUpTest {
+public class BecomeManagerTest extends AcmePlannerTest {
 
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/becomeManager/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	public void positiveBecomeManager(final String company, final String sector) {
-		super.signUp("username", "password", "name", "name", "user@mail.com");
-
+		super.navigateHome();
+		super.clickAndGo(By.linkText("Sign up"));
+		super.fill(By.id("username"), "username");
+		super.fill(By.id("password"), "password");
+		super.fill(By.id("confirmation"), "password");
+		super.fill(By.id("identity.name"), "name");
+		super.fill(By.id("identity.surname"), "surname");
+		super.fill(By.id("identity.email"), "email@gmail.com");
+		super.clickAndGo(By.id("accept$proxy"));
+		super.clickOnSubmitButton("Sign up");
+		
 		super.signIn("username", "password");
 		super.clickOnMenu("Account", "Become a manager");
 
