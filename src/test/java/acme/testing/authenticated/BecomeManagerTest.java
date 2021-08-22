fdd284d6618678704c41b2a183d44ed735ceb8e2
Alejandro Baños Gonzalez <alebangon@alum.us.es>
2021-08-22 18:57:45 +0200

    Arreglado test BecomeManager

--- src/test/java/acme/testing/authenticated/BecomeManagerTest.java
+++ src/test/java/acme/testing/authenticated/BecomeManagerTest.java
@@ -49,7 +49,7 @@ public class BecomeManagerTest extends AcmePlannerTest {
 	public void testBecomeManagerNegative(final String company, final String sector) {
 
 		super.signIn("authenticated", "authenticated");
-		super.clickOnMenu("Authenticated", "Become a manager");
+		super.clickOnMenu("Account", "Become a manager");
 
 		super.fillInputBoxIn("company", company);
 		super.fillInputBoxIn("sector", sector);
